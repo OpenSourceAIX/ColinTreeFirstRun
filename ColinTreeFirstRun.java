08ee0d146e1c68ea5828dcbff045108f3bcf833d
ColinTree <502470184@qq.com>
2017-08-17 20:32:59 +0800

    rewrite the code & update to the new format

--- ColinTreeFirstRun.java
+++ ColinTreeFirstRun.java
@@ -1,9 +1,4 @@
-/**
- * @author ColinTree (colinycl123@gmail.com)
- * @original appinventor.components.src.com.google.appinventor.components.runtime.TinyDB
- */
-
-package cn.colintree.aix;
+package cn.colintree.aix.ColinTreeFirstRun;
 
 import android.content.Context;
 import android.util.Log;
@@ -24,73 +19,71 @@ import org.json.JSONException;
 
 @SimpleObject(external = true)
 
-public class ColinTreeFirstRun extends AndroidNonvisibleComponent
-implements Component {
+public class ColinTreeFirstRun extends AndroidNonvisibleComponent implements Component {
     public static final int VERSION = 1;
     private ComponentContainer container;
     private Context context;
     private SharedPreferences sharedPreferences;
     private static final String LOG_TAG = "ColinTreeFirstRun";
+    private FirstRun firstRun;
     public ColinTreeFirstRun(ComponentContainer container) {
         super(container.$form());
         this.container = container;
         context = (Context) container.$context();
         Log.d(LOG_TAG, "FirstRun Created" );
-        sharedPreferences = context.getSharedPreferences("ColinTreeFirstRun", Context.MODE_PRIVATE);
+        firstRun=new FirstRun(container);
     }
     
-    final String tag="ColinTreeFirstRun";
-    final Object valueToStore="false";
-    
     @SimpleFunction(description = "return if it is first time to run, if it is true, set a flag.")
-    public String IsFirstRun() {
-    	if (GetValue(tag,"FirstRun!!!!!")=="FirstRun!!!!!"||GetValue(tag,"FirstRun!!!!!")=="false"){
-    		StoreValue(tag,valueToStore);
-    		return "true";
-    	}else{
-    		return "false";
-    	}
+    public boolean IsFirstRun() {
+        return firstRun.IsFirstRun();
     }
     @SimpleFunction(description = "")
     public void ClearFirstRunFlag() {
-    	final SharedPreferences.Editor sharedPrefsEditor = sharedPreferences.edit();
-    	sharedPrefsEditor.clear();
-    	sharedPrefsEditor.commit();
+        firstRun.ClearFirstRunFlag();
     }
-    
-    /**
-    * Store the given value under the given tag.  The storage persists on the
-    * phone when the app is restarted.
-    *
-    * @param tag The tag to use
-    * @param valueToStore The value to store. Can be any type of value (e.g.
-    * number, text, boolean or list).
-    */
-    public void StoreValue(final String tag, final Object valueToStore) {
-    	final SharedPreferences.Editor sharedPrefsEditor = sharedPreferences.edit();
-    	try {
-    		sharedPrefsEditor.putString(tag, JsonUtil.getJsonRepresentation(valueToStore));
-    		sharedPrefsEditor.commit();
-    	} catch (JSONException e) {
-    		throw new YailRuntimeError("Value failed to convert to JSON.", "JSON Creation Error.");
-    	}
-    }
-    /**
-    * Retrieve the value stored under the given tag.  If there's no such tag, then return valueIfTagNotThere.
-    *
-    * @param tag The tag to use
-    * @param valueIfTagNotThere The value returned if tag in not in TinyDB
-    * @return The value stored under the tag. Can be any type of value (e.g.
-    * number, text, boolean or list).
-    */
-    public Object GetValue(final String tag, final Object valueIfTagNotThere) {
-    	try {
-    		String value = sharedPreferences.getString(tag, "");
-    		// If there's no entry with tag as a key then return the empty string.
-    		//    was  return (value.length() == 0) ? "" : JsonUtil.getObjectFromJson(value);
-    		return (value.length() == 0) ? valueIfTagNotThere : JsonUtil.getObjectFromJson(value);
-    	} catch (JSONException e) {
-    		throw new YailRuntimeError("Value failed to convert from JSON.", "JSON Creation Error.");
-    	}
+
+    public class FirstRun{
+        final String tag="ColinTreeFirstRun";
+        final Object valueToStore="false";
+
+        private SharedPreferences sharedPreferences;
+        
+        public FirstRun(ComponentContainer container) {
+            sharedPreferences = context.getSharedPreferences("ColinTreeFirstRun", Context.MODE_PRIVATE);
+        }
+        
+        public boolean IsFirstRun() {
+            if (GetValue(tag,"FirstRun!!!!!")=="FirstRun!!!!!"||GetValue(tag,"FirstRun!!!!!")==valueToStore){
+                StoreValue(tag,valueToStore);
+                return true;
+            }
+            return false;
+        }
+        public void ClearFirstRunFlag() {
+            final SharedPreferences.Editor sharedPrefsEditor = sharedPreferences.edit();
+            sharedPrefsEditor.clear();
+            sharedPrefsEditor.commit();
+        }
+        
+        private void StoreValue(final String tag, final Object valueToStore) {
+            final SharedPreferences.Editor sharedPrefsEditor = sharedPreferences.edit();
+            try {
+                sharedPrefsEditor.putString(tag, JsonUtil.getJsonRepresentation(valueToStore));
+                sharedPrefsEditor.commit();
+            } catch (JSONException e) {
+                throw new YailRuntimeError("Value failed to convert to JSON.", "JSON Creation Error.");
+            }
+        }
+        
+        private Object GetValue(final String tag, final Object valueIfTagNotThere) {
+            try {
+                String value = sharedPreferences.getString(tag, "");
+                return (value.length() == 0) ? valueIfTagNotThere : JsonUtil.getObjectFromJson(value);
+            } catch (JSONException e) {
+                throw new YailRuntimeError("Value failed to convert from JSON.", "JSON Creation Error.");
+            }
+        }
+        
     }
 }
